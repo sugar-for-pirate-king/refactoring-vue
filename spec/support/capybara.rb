c0d00bffbe4d8ff96dfc47db96823d01c0fe6c44
philiplambok <philip.geraldi@harukaedu.com>
2019-10-25 22:29:38 +0700

    Initial commit

---
+++ spec/support/capybara.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.configure do |config|
+  chrome = Selenium::WebDriver::Chrome::Service
+  chrome.driver_path = Rails.root.join('spec', 'web_drivers', 'chromedriver')
+
+  config.before(:each, type: :system) do
+    driven_by :rack_test
+  end
+
+  config.before(:each, type: :system, js: true) do
+    driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1050]
+  end
+end
